cee4cecfad649e185e770a8f5b2870e39d2c2341
nlkassad <nlkassad@hotmail.com>
2016-09-29 23:38:45 -0400

    Add User Auth Handlers to Dishes Test rspec
    Add auth handlers from the user rspec and add to the dishes testing
    file. This adds testing for return on unsecured data.

--- spec/requests/dishes_spec.rb
+++ spec/requests/dishes_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Dishes API' do
+  def user_params
+    {
+      email: 'alice@example.com',
+      password: 'foobarbaz',
+      password_confirmation: 'foobarbaz'
+    }
+  end
+
   def dish_params
     {
-      name: 'Nachos'
+      name: 'Cheese Burger'
     }
   end
 
@@ -21,15 +29,66 @@ RSpec.describe 'Dishes API' do
 
   after(:all) do
     Dish.delete_all
+    User.delete_all
   end
 
-  describe 'GET /dishes' do
-    it 'lists all dishes' do
-      get '/dishes', nil, headers
-
-      expect(response).to be_success
-      dishes_response = JSON.parse(response.body)
-      expect(dishes_response['dishes']).not_to be_empty
+  context 'when authenticated' do
+    def headers
+      {
+        'HTTP_AUTHORIZATION' => "Token token=#{@token}"
+      }
     end
+
+    before(:each) do
+      post '/sign-up', credentials: user_params
+      post '/sign-in', credentials: user_params
+
+      @token = JSON.parse(response.body)['user']['token']
+      @user_id = JSON.parse(response.body)['user']['id']
+    end
+
+    describe 'GET /dishes' do
+      it 'is successful' do
+        get '/dishes', nil, headers
+
+        expect(response).to be_success
+
+        parsed_response = JSON.parse(response.body)
+        expect(
+          parsed_response['dishes']
+        ).not_to be_empty
+      end
+    end
+
+    # describe 'GET /users/:id' do
+    #   it 'is successful' do
+    #     get "/users/#{@user_id}", nil, headers
+    #
+    #     expect(response).to be_success
+    #
+    #     parsed_response = JSON.parse(response.body)
+    #     expect(
+    #       parsed_response['user']
+    #     ).not_to be_empty
+    #   end
+    # end
+  end
+
+  context 'when not authenticated' do
+    describe 'GET /dishes' do
+      it 'is not successful' do
+        get '/dishes'
+
+        expect(response).not_to be_success
+      end
+    end
+
+    # describe 'GET /users/:id' do
+    #   it 'is not successful' do
+    #     get "/users/#{@user_id}"
+    #
+    #     expect(response).not_to be_success
+    #   end
+    # end
   end
 end
